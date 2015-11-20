32b4ba3586f1e52bbb2d048b731986e726df03c7
mwhouser <matt@eleven41.com>
2015-11-20 15:06:29 -0500

    Initial revision

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ReplaceAppSetting
 PowerShell script to replace an appSetting value.
+
+## Main Script File
+
+[ReplaceAppSetting.ps1](blob/master/ReplaceAppSetting/ReplaceAppSetting.ps1)
+
+## Executing the Script
+
+```powershell
+ReplaceAppSetting.ps1 -sourceFile Source.config -targetFile Target.config -key keyToMatch -value newValue
+```
+
+## Modifying and Building
+
+This script was created using the following:
+
+* Visual Studio 2013
+* [PowerShell Tools for Visual Studio 2013](https://visualstudiogallery.msdn.microsoft.com/f65f845b-9430-4f72-a182-ae2a7b8999d7)
+
+## Credits
+
+* http://kevsoft.net/2015/09/18/using-powershell-to-replace-appsettings.html
---
+++ ReplaceAppSetting/ReplaceAppSetting.ps1
@@ -0,0 +1,29 @@
+﻿# ReplaceAppSetting.ps1
+# Based on http://kevsoft.net/2015/09/18/using-powershell-to-replace-appsettings.html
+
+param([Parameter(Mandatory=$True)][string]$sourceFile,
+    [Parameter(Mandatory=$True)][string]$targetFile,
+    [Parameter(Mandatory=$True)][string]$key,
+    [Parameter(Mandatory=$True)][string]$value)
+
+$version = "0.1.0"
+
+Write-Output "ReplaceAppSetting"
+Write-Output "Version $version"
+
+Write-Output "Loading config file from $sourceFile"
+$xml = [xml](Get-Content $sourceFile)
+
+ForEach($add in $xml.configuration.appSettings.add)
+{
+    if($add.key -eq $key)
+    {
+        Write-Output "Found matching environment variable for key: $($add.key)"
+        Write-Output "Replacing value '$($add.value)' with '$value'"
+
+        $add.value = $value
+    }
+}
+
+Write-Output "Writing to config file $targetFile"
+$xml.Save($targetFile)
